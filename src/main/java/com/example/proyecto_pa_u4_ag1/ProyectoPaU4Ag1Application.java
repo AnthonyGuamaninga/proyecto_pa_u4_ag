0a28ace006b5645236b63bcaf9dbcba412aa27cf
Anthony Guamaninga <anthonysub300@gmail.com>
2023-02-28 19:44:23 -0500

    Taller 36: Página web

---
+++ src/main/java/com/example/proyecto_pa_u4_ag1/ProyectoPaU4Ag1Application.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_pa_u4_ag1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaU4Ag1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaU4Ag1Application.class, args);
+	}
+
+}
