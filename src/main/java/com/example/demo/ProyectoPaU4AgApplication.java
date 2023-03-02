f8ab5bc45a4c2c4e0ffa1ff86824bf5e0be62424
Anthony Guamaninga <anthonysub300@gmail.com>
2023-03-02 00:53:37 -0500

    taller37

--- src/main/java/com/example/demo/ProyectoPaU4AgApplication.java
+++ src/main/java/com/example/demo/ProyectoPaU4AgApplication.java
@@ -26,23 +26,7 @@ public class ProyectoPaU4AgApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		Persona persona = new Persona();
-		persona.setApellido("Guamaninga");
-		persona.setNombre("Alfredo");
-		persona.setCedula("1753244034");
-		persona.setGenero("M");
-		this.iPersonaService.guardar(persona); // guardar
-		
-		Persona personaMod = this.iPersonaService.buscarPorId(2); // buscar
-		personaMod.setApellido("Ambuludi");
-		personaMod.setNombre("Steven");
-		personaMod.setCedula("1753200324");
-		this.iPersonaService.actualizar(personaMod); // actualizar
-		
-		this.iPersonaService.eliminar(3);
-		
-		List<Persona> personas = this.iPersonaService.buscarTodos();
-		personas.forEach(lista -> LOG.info(" "+lista)); // buscar Todos
+
 	}
 
 }
